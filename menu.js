05bd329711c8a5869b3bc76379da4eceba374d0d
PintsandPals <al@pintsandpals.co.uk>
2026-03-12 18:30:46 +0000

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,5 +1,16 @@
-
 function toggleMenu(){
-const menu = document.getElementById("nav-links");
-menu.classList.toggle("active");
+  const menu = document.getElementById("nav-links");
+  menu.classList.toggle("active");
+}
+
+// Expandable Image Functions
+function expandImage(img) {
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImg");
+  modal.style.display = "block";
+  modalImg.src = img.src;
+}
+
+function closeModal() {
+  document.getElementById("imageModal").style.display = "none";
 }
